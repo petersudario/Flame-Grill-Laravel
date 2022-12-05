632bb23928637dd995576688f533f54893ed311a
Pedro Henrique Sudario da Silva <84980739+petersudario@users.noreply.github.com>
2022-12-05 19:42:33 -0300

    Update contato.blade.php

--- resources/views/site/contato.blade.php
+++ resources/views/site/contato.blade.php
@@ -3,17 +3,38 @@
     @component('components.navbar')
     
     @endcomponent
-    <div class="bg-no-repeat p-4 bg-cover md:bg-center xl:bg-center sm:bg-center h-[800px] flex justify-center" style="background-image: url('{{asset('/img/pizzaWall.jpg')}}')" >
-        <div class="max-w-4xl min-w-xl grid gap-4 xs:grid-cols-1 xs:w-full md:grid-cols-1 sm:grid-cols-1 lg:grid-cols-0">
+    <div class="bg-no-repeat p-4 bg-cover md:bg-center xl:bg-center sm:bg-center h-full] flex justify-center" style="background-image: url('{{asset('/img/pizzaWall.jpg')}}')" >
+        <div class="max-w-4xl min-w-xl grid gap-4 xs:grid-cols-1 xs:w-full md:grid-cols-1 sm:grid-cols-1 lg:grid-cols-1">
             <div class="flex">
                 <div class="pt-40 pb-40 pl-[30px] pr-[30px] md:pl-10 md:pr-40 sm:pl-5 sm:pr-40 xs:pl-[30px] xs:pr-[30px]">
                     <div>
                         <h1 style="text-shadow: 1px 1px 7px #151212" class="text-3xl lg:text-4xl xs:text-3xl text-justify tracking-tight text-white font-serif">    FORMULÁRIO</h1>
                             <br>
-                        <h2 style="text-shadow: 1px 1px 7px #151212" class="text-white text-justify tracking-tight lg:text-xl xs:text-sm ">Favor entrar em contato para qualquer dúvida, reclamação ou sugestão, preenchendo o formulário abaixo.</h2>
-                        <br><br>
-                        <form>
-                            <input class="w-[600px]" type="text" name="nome" placeholder="Nome completo">
+                        <p style="text-shadow: 1px 1px 7px #151212" class="text-white text-justify tracking-tight lg:text-xl xs:text-sm">Favor entrar em contato para qualquer dúvida, reclamação ou sugestão, preenchendo o formulário abaixo com suas informações de contato e o assunto abordado.</p>
+                        <form class="text-white px-4 my-10 max-w-3xl mx-auto space-y-6">
+                            <div class="grid 2xs:grid-cols-2 gap-4">
+                                <div>
+                                    <label class="text-xl" for="nome">Nome completo</label>
+                                    <input class="border border-gray-400 py-2  px-4 w-full rounded focus:outline-none text-black" type="text" name="nome">
+                                </div>
+                                <div>
+                                    <label class="text-xl" for="telefone">Telefone</label>
+                                    <input class="border border-gray-400 py-2  px-4 w-full rounded focus:outline-none text-black" type="text" name="telefone" placeholder="Ex: +55 (41) 90000-0000">
+                                </div>
+                            </div>
+                            <div class="grid grid-cols-1 gap-4">
+                                <div>
+                                    <label class="text-xl" for="email">E-mail</label>
+                                    <input class="border border-gray-400 py-2  px-4 w-full rounded focus:outline-none text-black" type="text" name="email" placeholder="Ex: comida@gmail.com">
+                                </div>
+                            </div>
+                            <div class="grid grid-cols-1 gap-4">
+                                <div>
+                                    <label class="text-xl" for="telefone">Escreva abaixo sua mensagem</label>
+                                    <input class="border border-gray-400 py-2  px-4 w-full rounded focus:outline-none text-black" type="text" name="telefone">
+                                </div>
+                            </div>
+                            <input type="submit" class="bg-black text-white p-4 rounded-lg" style="cursor: pointer">	
                         </form>
                     </div>
                 </div>
