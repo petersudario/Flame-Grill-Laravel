b8f6988865c7d566804233b67d4b6cbee6fcaad9
Pedro Sudario <pedrohenriquesudariosilva@gmail.com>
2024-09-02 22:16:55 -0300

    test

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,6 +1,8 @@
 <head>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta charset="utf-8">
+  <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+
   @vite('resources/css/app.css')
   <script src="//unpkg.com/alpinejs" defer></script>
   <link rel="shortcut icon" href="{{ asset('img/flame-shape-svgrepo-com.svg') }}"/>
